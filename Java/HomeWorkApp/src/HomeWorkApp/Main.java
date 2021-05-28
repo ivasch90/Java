71a88d925617386f25a45edf8a7f651137ddc21b
ivasch <melmett@yandex.ru>
2021-05-28 23:47:24 +0300

    updated 4.22

--- Java/HomeWorkApp/src/HomeWorkApp/Main.java
+++ Java/HomeWorkApp/src/HomeWorkApp/Main.java
@@ -44,11 +44,11 @@ public class Main {
         int [][] arr = new int[5][5];
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
-                if (i == j || i == 0 && j == arr.length - 1 || j == 0 && i == arr.length - 1)
+                    if (i == j || (i + j) == arr.length - 1)
                     arr[i][j] = 1;
-                else if (i == 1 && j == arr.length - 2 || j == 1 && i == arr.length - 2)
-                    arr[i][j] = 1;
-                System.out.print(arr[i][j] + "   ");
+
+                    System.out.print(arr[i][j] + "   ");
+
             }
             System.out.println();
         }
