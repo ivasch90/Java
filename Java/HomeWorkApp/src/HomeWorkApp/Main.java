324e8a82d6879d84892fbb9a1ada936f5e93aa40
ivasch <melmett@yandex.ru>
2021-06-06 18:45:00 +0300

    lesson-7: 1,2,3,4,5 completeddd

--- Java/HomeWorkApp/src/HomeWorkApp/Main.java
+++ Java/HomeWorkApp/src/HomeWorkApp/Main.java
@@ -8,9 +8,25 @@ public class Main {
         plate.info();
         cat.eat(plate);
         plate.info();
+        System.out.println("=============================");
+        catArrayMethod();
 
     }
-    //ublic void catArrayMethod() {
 
-   // }
+    public static void catArrayMethod() {
+        cat[] catArray = new cat[5];
+        catArray[0] = new cat("kisa", 10);
+        catArray[1] = new cat("murzik", 5);
+        catArray[2] = new cat("john", 7);
+        catArray[3] = new cat("lisa", 6);
+        catArray[4] = new cat("yellow", 9);
+
+        plate plate = new plate(29);
+        plate.info();
+        for (int i = 0; i < catArray.length; i++) {
+            catArray[i].eat(plate);
+            plate.info();
+        }
+
+    }
 }
--- Java/HomeWorkApp/src/HomeWorkApp/plate.java
+++ Java/HomeWorkApp/src/HomeWorkApp/plate.java
@@ -1,5 +1,7 @@
 package HomeWorkApp;
 
+import java.util.Scanner;
+
 public class plate {
     private int food;
     boolean eatFood = false;
@@ -7,11 +9,6 @@ public class plate {
     public plate(int food) {
         this.food = food;
     }
-
-    public int getFood() {
-        return food;
-    }
-
 
     public void info() {
         System.out.println("Plate: " + food);
@@ -25,9 +22,17 @@ public class plate {
         else if (food < 0){
             System.out.println("Error");
         }
+        else eatFood = false;
     }
 
     public boolean isEatFood() {
         return eatFood;
+    }
+
+    public void addPlate() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please add food to plate [1...] ");
+        food += scanner.nextInt();
+        scanner.close();
     }
 }
