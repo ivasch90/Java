305ac238b80af8a63c09a0440f3d7859e6b7a655
ivasch <melmett@yandex.ru>
2021-07-11 16:11:26 +0300

    updated v 1.2

--- src/HomeWorkApp/Main.java
+++ src/HomeWorkApp/Main.java
@@ -1,7 +1,13 @@
 package HomeWorkApp;
 
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Main {
-    static final byte NUMBER_TRUE = 4;
+    private static final byte NUMBER_TRUE = 4;
+
+
     public static void main(String[] args) {
 
 
@@ -11,15 +17,15 @@ public class Main {
         array[0][2] = "lesson";
         array[0][3] = "good";
         array[1][0] = "lose";
-        array[1][1] = "io";
+        array[1][1] = String.valueOf(5);
         array[1][2] = "weather";
         array[1][3] = "fly";
-        array[2][0] = "pop";
+        array[2][0] = String.valueOf(10);
         array[2][1] = "senior";
         array[2][2] = "middle";
         array[2][3] = "junior";
         array[3][0] = "ghost";
-        array[3][1] = "react";
+        array[3][1] = String.valueOf(2);
         array[3][2] = "super";
         array[3][3] = "slim";
 
@@ -27,40 +33,46 @@ public class Main {
         String[][] mount = new String[2][3];
 
         try {
-            arraySumException(mount);
-            arraySumException(qwerty);
+            //arraySumException(mount);
+            //arraySumException(qwerty);
             arraySumException(array);
 
-        } catch (MyArraySizeException | MyArrayDataException e) {
+        } catch (MyArraySizeException  | MyArrayDataException e) {
             e.printStackTrace();
-
-        }
-        finally {
-            System.out.println("Array size " + mount.length + " is not " + NUMBER_TRUE);
-            System.out.println("Array size " + qwerty.length + " is not " + NUMBER_TRUE);
 
         }
 
 
     }
 
-    public static void arraySumException(String[][] array) throws MyArraySizeException, MyArrayDataException {
+    public static void arraySumException(String[][] array) throws MyArrayDataException {
 
         if (array.length == NUMBER_TRUE) {
             for (int i = 0; i < array.length; i++) {
                 for (int j = 0; j < array[i].length; j++) {
                     int sum = 0;
                     try {
-                        sum = +Integer.parseInt(array[i][j]);
-                        System.out.println(sum);
+                        sum =+ Integer.parseInt(array[i][j]);
+
+                    } catch (MyArrayDataException e) {
+                        e.printStackTrace();
                     } catch (NumberFormatException e) {
-                        //e.printStackTrace();
-                        System.out.println("NumberFormatException " + array + "[" + i + "]" + "[" + j + "]");
+                        System.out.println("NumberFormatException in array " + "[" + i + "]" + "[" + j + "]");
                     }
 
                 }
             }
-        }
-
-        }
+        } else if (array.length > NUMBER_TRUE) {
+            try {
+                throw new MyArraySizeException();
+            } catch (MyArraySizeException e) {
+                System.out.println("Array size " + array.length + " > " + NUMBER_TRUE);
+            }
+        } else
+            try {
+                throw new MyArraySizeException();
+            } catch (MyArraySizeException e) {
+                System.out.println("Array size " + array.length + " < " + NUMBER_TRUE);
+            }
+    }
 }
--- src/HomeWorkApp/MyArraySizeException.java
+++ src/HomeWorkApp/MyArraySizeException.java
@@ -1,16 +1,14 @@
 package HomeWorkApp;
 
-public class MyArraySizeException extends Exception {
-
+public class MyArraySizeException extends ArrayIndexOutOfBoundsException {
     public MyArraySizeException() {
-        System.out.println("Size of Array is not valid...");
     }
 
-    public MyArraySizeException(String message) {
-        super(message);
+    public MyArraySizeException(String s) {
+        super(s);
     }
 
-    public MyArraySizeException(String message, Throwable cause) {
-        super(message, cause);
+    public MyArraySizeException(int index) {
+        super(index);
     }
 }
