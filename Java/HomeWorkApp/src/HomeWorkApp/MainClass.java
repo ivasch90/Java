979b444f122e312d04a2707198945dd78cc74f3e
ivasch <melmett@yandex.ru>
2021-10-15 11:58:07 +0300

    Complete...Race Cars

---
+++ Java/HomeWorkApp/src/HomeWorkApp/MainClass.java
@@ -0,0 +1,40 @@
+package HomeWorkApp;
+
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
+public class MainClass {
+    public static final int CARS_COUNT = 4;
+    static CyclicBarrier cyclicBarrier = new CyclicBarrier(CARS_COUNT);
+    static CountDownLatch cdl = new CountDownLatch(CARS_COUNT);
+    static CountDownLatch downLatch = new CountDownLatch(CARS_COUNT);
+
+    public static void main(String[] args) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+        }
+
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+        }
+
+        try {
+            cdl.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        try {
+            downLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+}
